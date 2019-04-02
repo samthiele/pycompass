3d765892958be7ba6dd87aa244426754f760adfa
Sam Thiele <sam.thiele01@gmail.com>
2019-04-02 21:21:13 +1100

    Added CombinedSNEList class
    Added CombinedSNEList class for managing data from one field site (for example). This is different to merging all SNELists as equal weighting is given to each list during KDE calculation (rather than being biased towards structures that have more SNEs).

--- SNE/__init__.py
+++ SNE/__init__.py
@@ -92,6 +92,40 @@ class SNEList:
         self.thickKDE = None
         self.oriKDE = None
     
+    """
+    Bins data in this SNEList 
+
+    **Arguments**:
+     - vals = a list of the same length as this SNEList containg the value to decide which bin each measurement belongs
+     - binwidth = the size of each bin
+     - minv = the value to start binning at
+     - maxv = the value to end binning at
+
+    **Returns**:
+     - bins = the lower boundary of each bin
+     - values = a SNEList object containing the data in this bin, or None if no SNEs fall within it. 
+    """
+    def bin(self,vals,binwidth,minv,maxv):
+        vals = np.array(vals)
+        bins = np.arange(minv,maxv,binwidth)
+        values = [None for n in range(len(bins))]
+        
+        #loop through bins
+        for i in range(len(bins)):
+            _min = bins[i]
+            _max = bins[i] + binwidth
+            mask = np.logical_and(vals > _min, vals < _max )
+            if sum(mask) > 0:
+                values[i] = SNEList(self.pos[0,mask],
+                                    self.pos[1,mask],
+                                    self.pos[2,mask],
+                                    self.trend[mask],
+                                    self.plunge[mask],
+                                    self.thickness[mask])
+        
+        #return
+        return bins,values
+    
     ############################
     ##STATISTICAL INTERROGATION
     ############################
@@ -102,7 +136,7 @@ class SNEList:
     **Arguments**:
      -variable = the KDE to compute/retrieve. Should be "strike", "dip", "trend", "plunge" or "thickness" (1D KDEs) or "orientation" (2D).
     **Keywords**:
-     -bw = the bandwidth of the kde to calculate. Default is 2.5 degrees for angular data and 2.5 cm for thickness data. 
+     -bw = the bandwidth of the kde to calculate. Default is 2.5 degrees for angular data and 5.0 cm for thickness data. 
      -recalc = if True, this forces the KDE to be recalculated. 
     **Returns**:
      -grid = the grid points associated with each KDE. Will be a 1D list of x coords for 1D KDEs and a 2D grid for "orientation".
@@ -112,7 +146,7 @@ class SNEList:
     def getKDE(self,variable,**kwds):
         
         #get bandwidth
-        bw = kwds.get("bw",2.5)
+        bw = kwds.get("bw",5.0)
         recalc = kwds.get("recalc",False)
         
         if "trend" in variable.lower():
@@ -167,13 +201,18 @@ class SNEList:
                 gmin = kwds.get("gmin",0)
                 gmax = kwds.get("gmax",np.max(self.thickness))
                 res = kwds.get("res",1000)
-                bw = kwds.get("bw",0.025)
+                bw_method = kwds.get("bw",0.025)
+                
+                #calculate bw for the gaussian kernal
+                #(scipy's definition the kernel standard deviation = passed bandwidth * standard deviation of the sample
+                if not bw is None:
+                    bw_method = bw / np.std(self.thickness)
                 
                 #build grid
                 self.thickGrid = np.linspace(gmin,gmax,res)
 
                 #evaluate KDE
-                self.thickKDE = scipy.stats.gaussian_kde(self.thickness,bw_method=bw)(self.thickGrid)
+                self.thickKDE = scipy.stats.gaussian_kde(self.thickness,bw_method=bw_method)(self.thickGrid)
                 
             return np.array(self.thickGrid), np.array(self.thickKDE)
         
@@ -330,3 +369,162 @@ class SNEList:
         cs = ax1.contourf(X,Y,kde2D,**kwds)
 
         return fig, ax1      
+        
+        
+        
+        
+"""
+Class for combining multiple SNELists (each representing different structures) with equal weighting (rather than just merging the SNEs, which weights based on the number of SNEs for each structure).
+"""
+class CombinedSNEList:
+    """
+    Init a combinedSNEList object.
+    
+    **Arguments**:
+     - name = a name for this set of structures
+     - SNEs = a list of SNEList objects which will be combined with equal weighting. 
+    """
+    def __init__(self, name, SNEs):
+        self.name = name
+        self.SNEs = SNEs
+        assert len(SNEs) > 0, "Error -CombinedSNEList must contain at least one SNEList...."
+    """
+    Calculates a combined KDE by summing KDEs of each structure in this combined SNE List.
+    
+    **Arguments**:
+     -variable = the KDE to compute/retrieve. Should be "strike", "dip", "trend", "plunge" or "thickness" (1D KDEs) or "orientation" (2D).
+    **Keywords**:
+     -bw = the bandwidth of the kde to calculate. Default is 2.5 degrees for angular data and 5.0 cm for thickness data. 
+     -recalc = if True, this forces the KDE to be recalculated. 
+     -gmin,gmax = required for thickness kdes to specify range of values to evaluate kde over. 
+    **Returns**:
+     -grid = the grid points associated with each KDE. Will be a 1D list of x coords for 1D KDEs and a 2D grid for "orientation".
+     -KDE = the kernel density estimates associated with each grid point. 
+    """
+    def getKDE(self,variable,**kwds):
+        
+        #set default gmin/gmax for thickness kde
+        if "thick" in variable:
+            kwds["gmin"] = kwds.get("gmin",0.0)
+            kwds["gmax"] = kwds.get("gmax",10)
+        
+        #get grid and KDE from first SNE
+        grid,kde = self.SNEs[0].getKDE(variable,**kwds)
+        
+        #sum all others onto original (thus forcing equal weighting)
+        for i in range(1,len(self.SNEs)):
+            _,_k = self.SNEs[i].getKDE(variable,**kwds)
+            kde += _k
+            
+        #normalise    
+        kde /= len(self.SNEs)
+        
+        return grid,kde
+    
+    
+    """
+    Evaluate probabilities by integrating the strike, dip or thickness KDEs. Note that this doesn't wrap angular data...
+    
+    **Arguments**:
+     -variable = the variable to evaluate. Should be a string that matches "strike", "dip" or "thickness". 
+     -minv = the minima of the section of pdf to integrate. Must range between 0 and 360 for strike/trend and 0 to 90 for dip. 
+     -maxv = the maxima of the section of pdf to integrate. Must range between 0 and 360 for strike/trend and 0 to 90 for dip. 
+    
+    **Keywords**:
+     -keywords are passed to the getKDE(....=) function.
+    """
+    def evalP1D( self, variable, minv, maxv, **kwds ):
+        
+        assert minv < maxv, "Don't be stupid (or, if evaluating angles such as the sector 315 to 045, split into two sections)."
+        
+        #get grid, kde
+        grid,kde = self.getKDE(variable)
+        
+        #special case: strike ranges 0 to 180 (not 0 to 360), so we need to double kde vals
+        if "strike" in variable.lower():
+            kde *= 2
+            
+        #mask values to integrate
+        mask = np.logical_and(grid >= minv,grid <= maxv)
+        
+        #integrate and return
+        return np.trapz(kde[mask],grid[mask])     
+        
+    """
+    Plot a 1D KDE. 
+    
+    **Arguments**:
+     -variable = the variable to plot. Should be a string that matches "strike", "dip" or "thickness". 
+    """
+    def plotKDE1D( self, variable, **kwds ):
+        fig = plt.figure()
+        grid,kde = self.getKDE(variable,**kwds)
+        plt.plot(grid,kde)
+        return fig, plt.gca() 
+        
+    """
+    Plot a rose diagram of the "strike" KDE.
+    
+    **Keywords**:
+     - plotArgs = a dict of keywords to pass to the plot(...) function to modify the rose diagram outline.
+     - fillArgs = a dict of keywords to pass to the fill(...) function to modify rose diagram fill. 
+     
+    **Returns**:
+     - fig, ax = the figure and axes containing the rose diagram. 
+    """
+    def plotRose( self, **kwds ):
+        _theta,kde = self.getKDE("strike",**kwds)
+        
+        #make figure?
+        if "ax" in kwds:
+            ax = kwds.get("ax")
+            fig = ax.figure
+        else:
+            fig = plt.figure(figsize=(4,4))
+            ax = fig.add_subplot(111, projection='polar')
+
+        #get args
+        plotArgs = kwds.get("plotArgs",{"color":'k'})
+        fillArgs = kwds.get("fillArgs",{"color":'gray',"alpha":0.25})
+        
+        #plot
+        ax.plot( np.deg2rad(_theta),kde, **plotArgs)
+        ax.fill( np.deg2rad(_theta),kde, **fillArgs)
+        ax.set_theta_zero_location('N')
+        ax.set_theta_direction(-1)
+        ax.get_yaxis().set_visible(False)
+        return fig, ax
+    
+    
+    """
+    Plot a 2D orientation KDE on a stereonet.
+    
+    **Keywords**:
+     - kwds are passed directly to plt.contourf(...).  
+     
+    **Returns**:
+     - fig, ax = the figure and axes containing the rose diagram. 
+     
+    """
+    def plotStereonet( self, **kwds ):
+        #get kde
+        grid,kde = self.getKDE("orientation",degrees=False,**kwds)
+
+        #reshape kde into a grid
+        res = int(np.sqrt(grid.shape[1]))
+        assert res == np.sqrt(grid.shape[1]), "Error plotting KDE - supplied grid must be square."
+        kde2D = np.reshape(kde,(res,res)).T
+
+        #build meshgrid of plotting coordinates (in lat,lon)
+        bound = np.pi / 2
+        X,Y = np.meshgrid(np.linspace(-bound,bound,res), np.linspace(-bound,bound,res)) #rectangular plot of polar data
+
+        #plot
+        fig = plt.figure(figsize=(8,8))
+        ax1 = fig.add_subplot(111, projection='stereonet')
+        
+        if not "cmap" in kwds:
+            kwds["cmap"] = "coolwarm"
+        cs = ax1.contourf(X,Y,kde2D,**kwds)
+
+        return fig, ax1
