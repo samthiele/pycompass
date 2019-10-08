3643802662f0cbf2907a787fcfe70f2130506fb9
Lachlan Grose <lachlan.grose@monash.edu>
2019-10-08 11:25:33 +1100

    making installer work

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 #setup cython code
 import sys, os
-from distutils.core import setup
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy as np
 import scipy as sp
 
-os.chdir('..') #move up one level
+#os.chdir('..') #move up one level
 setup(
-	name = "pdf",
+	name = "pycompass",
+        install_requires=['Cython'],
+        packages=find_packages(),
 	ext_modules = cythonize("pycompass/SNE/pdf.pyx"),
 	include_dirs = [np.get_include(),sp.get_include()]
 	)
